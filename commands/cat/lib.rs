48466a7c0cea9951301c1e2f2ef1a35cecb306ba
linbingquan <695601626@qq.com>
2022-03-22 19:44:30 +0800

    feat(help): print help

--- commands/cat/lib.rs
+++ commands/cat/lib.rs
@@ -3,51 +3,51 @@ use core::{App, Command};
 struct Cat {}
 
 impl Cat {
-  fn new() -> Self {
-    Cat {}
-  }
-  fn cat(&self, path: &str) -> Result<String, std::io::Error> {
-    std::fs::read_to_string(path)
-  }
+    fn new() -> Self {
+        Cat {}
+    }
+    fn cat(&self, path: &str) -> Result<String, std::io::Error> {
+        std::fs::read_to_string(path)
+    }
 }
 
 impl Command for Cat {
-  fn execute(&self, args: Vec<String>) {
-    if args.len() != 2 {
-      println!("需要输入路径, 且只能输入一个路径, 如果你雀食只输入了一个路径, 那应该是框架的转换参数的地方有问题");
-      return;
+    fn execute(&self, args: Vec<String>) {
+        if args.len() != 2 {
+            println!("需要输入路径, 且只能输入一个路径, 如果你雀食只输入了一个路径, 那应该是框架的转换参数的地方有问题");
+            return;
+        }
+        let path = &args[1];
+        let r = self.cat(path);
+        match r {
+            Ok(text) => println!("{}", text),
+            Err(e) => println!("{}", e),
+        }
     }
-    let path = &args[1];
-    let r = self.cat(path);
-    match r {
-      Ok(text) => println!("{}", text),
-      Err(e) => println!("{}", e),
-    }
-  }
 
-  fn help(&self) {
-    println!("cat README.md");
-  }
+    fn help(&self) {
+        println!("cat README.md");
+    }
 }
 
 pub fn init(app: &mut App) {
-  app.register(String::from("cat"), Box::new(Cat::new()));
+    app.register(String::from("cat"), Box::new(Cat::new()));
 }
 
 #[cfg(test)]
 mod tests {
-  #[test]
-  fn test_ok() {
-    use crate::Cat;
-    let cat = Cat::new();
-    let test = cat.cat("test.txt").unwrap();
-    assert_eq!(test, "test data");
-  }
-  #[test]
-  fn test_panic() {
-    use crate::Cat;
-    let cat = Cat::new();
-    let result = cat.cat("no_file.txt");
-    assert!(result.is_err())
-  }
+    #[test]
+    fn test_ok() {
+        use crate::Cat;
+        let cat = Cat::new();
+        let test = cat.cat("test.txt").unwrap();
+        assert_eq!(test, "test data");
+    }
+    #[test]
+    fn test_panic() {
+        use crate::Cat;
+        let cat = Cat::new();
+        let result = cat.cat("no_file.txt");
+        assert!(result.is_err())
+    }
 }
--- frame/main.rs
+++ frame/main.rs
@@ -1,28 +1,34 @@
 use core::App;
-use std::io;
 
 mod input;
 
 fn main() {
-  // 新建 app
-  let mut app = App::default();
+    // 新建 app
+    let mut app = App::default();
 
-  /* 注册模块 start */
-  cat::init(&mut app);
-  /* 注册模块 end */
+    /* 注册模块 start */
+    cat::init(&mut app);
+    /* 注册模块 end */
 
-  loop {
-    // 等待用户输入
-    let args = input::get_input();
-    println!("args:\n{args:?}");
-    if args.len() < 1 {
-      continue;
+    loop {
+        // 等待用户输入
+        let args = input::get_input();
+
+        if args.is_empty() {
+            continue;
+        }
+
+        // TODO: 暂时先这样 后续再优化交互方式
+        // 输出帮助文档
+        if args[0] == "help" {
+            app.help();
+            continue;
+        }
+
+        // 获取命令
+        let command = app.get_command(&args[0]);
+
+        // 执行命令
+        command.execute(args);
     }
-
-    // 获取命令
-    let command = app.get_command(&args[0]);
-
-    // 执行命令
-    command.execute(args);
-  }
 }
